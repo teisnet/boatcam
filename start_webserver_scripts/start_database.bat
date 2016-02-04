fe4503a3910f74b11736a355ff270f0a651ae956
Teis Draiby <teis@teis.net>
2016-02-04 18:05:11 +0100

    Include mongodb as project local database

--- start_webserver_scripts/start_database.bat
+++ start_webserver_scripts/start_database.bat
@@ -2,7 +2,10 @@
 echo STARTING MONGODB SERVER
 
 SET MONGODB_PATH=C:\Program Files\MongoDB\Server\3.2\bin
-SET STARTUP_SCRIPT="%MONGODB_PATH%\mongod.exe"
+SET DB_PATH=..\mongodb\data\db
+SET CONFIG_PATH=..\mongodb\mongod.cfg
+SET LOG_PATH=..\mongodb\log\mongod.log
+SET STARTUP_SCRIPT=mongod.exe --config %CONFIG_PATH% --dbpath %DB_PATH% --logpath %LOG_PATH%
 
 %STARTUP_SCRIPT%
 
