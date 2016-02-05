bbeb6f41c171fcee70f31e671e2b7be28d7d499d
Teis Draiby <teis@teis.net>
2016-02-05 22:21:29 +0100

    Refactor into 'Camera' class, client side

---
+++ public/javascripts/camera-page.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function(){
+    $(".camera-control .up").mousedown("up",onMoveCamera);
+    $(".camera-control .left").mousedown("left",onMoveCamera);
+    $(".camera-control .right").mousedown("right",onMoveCamera);
+    $(".camera-control .down").mousedown("down",onMoveCamera);
+
+    $(".camera-control .stop").mousedown(stopCamera);
+    $(".camera-control .zoom-in").mousedown("zoomIn",onMoveCamera);
+    $(".camera-control .zoom-out").mousedown("zoomOut",onMoveCamera);
+
+    $(".savecamerapos").bind("click", function(){
+        var berthId = $('#berths').val();
+        $.post('api/berths/' + berthId + '/positions', Camera.status, function(res){ });
+    });
+
+    $(".loadcamerapos").bind("click", function(){
+        var berthId = $('#berths').val();
+        $.getJSON('api/berths/' + berthId + '/positions', function(res){
+            Camera.moveTo(res);
+        });
+    });
+});
+
+$.getJSON('api/berths', function(berthData) {
+    $(berthData).each(function (index, berth) {
+        var $option = $("<option/>").attr("value", berth._id).text(berth.number + " - " + berth.owner);
+        $('#berths').append($option);
+    });
+    /*$.each(data, function(index, berth)
+        var $select = $('#mySelectID');
+    });*/
+});
+
+$(Camera).on('move', function (event, pos) {
+    $('.status').text("x: " + pos.x + "\xB0 y: " + pos.y + "\xB0 zoom: " + pos.zoom + "x");
+});
+
+
+function onMoveCamera(event) {
+    Camera.move(event.data);
+    $(document).bind("mouseup", stopCamera);
+};
+
+function stopCamera() {
+    Camera.stop();
+    $(document).unbind("mouseup", stopCamera);
+}
--- public/javascripts/camera.js
+++ public/javascripts/camera.js
@@ -1,68 +1,35 @@
-var socket = io();
-var cameraStatus = {x: 0, y: 0, zoom: 0};
-console.log("Camera");
 
-socket.on("status", function(_sta){
-   cameraStatus = _sta;
-   var sta = {};
-    sta.x = parseFloat(_sta.x / 100).toFixed(1);
-    sta.y = parseFloat(_sta.y / 100).toFixed(1);
-    sta.zoom = parseFloat(_sta.zoom / 1000).toFixed(1);
-    
-    $('.status').text("x: " + sta.x + "\xB0 y: " + sta.y + "\xB0 zoom: " + sta.zoom + "x");
-  
-});
+var Camera = (function(){
 
-function beginMoveCamera(direction){
-    console.log("moveCamera: " + direction);
-    socket.emit('move', direction);
-    
-    $(document).bind("mouseup", stopCamera);
-}
+    var _camera = {};
 
-function moveCamera(pos) {
-    console.log("moveCamera: " + pos);
-    socket.emit('set', pos);
-}
+    var socket = io();
 
+    socket.on("status", function(_sta){
+        _camera.position = _sta;
+        var sta = {};
+            sta.x = parseFloat(_sta.x / 100.0).toFixed(1);
+            sta.y = parseFloat(_sta.y / 100.0).toFixed(1);
+            sta.zoom = parseFloat(_sta.zoom / 1000.0).toFixed(1);
 
-function stopCamera() {
-    socket.emit('move', "stop");
-    $(document).unbind("mouseup", stopCamera);
-}
-
-
-// API
-
-$.getJSON('api/berths', function(data) {
-    var $select = $('#berths');
-    $(data).each(function (index, o) {    
-        var $option = $("<option/>").attr("value", o._id).text(o.number + " - " + o.owner);
-        $select.append($option);
-    });
-    /*$.each(data, function(index, berth){
-        //console.log(berth);
-        var $select = $('#mySelectID');
-    })*/;
-});
-
-function onMoveCamera(event) {
-    beginMoveCamera(event.data);
-};
-
-$(document).ready(function(){
-    $(".camera-control .up").mousedown("up",onMoveCamera);
-    $(".camera-control .left").mousedown("left",onMoveCamera);
-    $(".camera-control .right").mousedown("right",onMoveCamera);
-    $(".camera-control .down").mousedown("down",onMoveCamera);
-
-    $(".savecamerapos").bind("click", function(){
-        var berthId = $('#berths').val();
-        $.post('api/berths/' + berthId + '/positions', cameraStatus, function(res){ });
+            $(_camera).triggerHandler("move", sta);
     });
 
-    $(".loadcamerapos").bind("click", function(){
-        var berthId = $('#berths').val();
-        $.getJSON('api/berths/' + berthId + '/positions', function(res){ moveCamera(res); });
-    });
-});
+    _camera.position = {x: 0, y: 0, zoom: 0};
+
+    _camera.move = function(direction){
+            console.log("Camera.move: " + direction);
+            socket.emit('move', direction);
+        }
+
+    _camera.moveTo = function(pos) {
+        console.log("Camera.moveTo: " + pos);
+        socket.emit('set', pos);
+    }
+
+    _camera.stop = function() {
+        socket.emit('move', "stop");
+    }
+
+    return _camera;
+})();
