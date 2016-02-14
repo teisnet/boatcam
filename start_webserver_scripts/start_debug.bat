288d8282ef98759bb037ab08e509bce4eedafae2
Teis Draiby <teis@teis.net>
2016-02-14 20:40:09 +0100

    Rename stite to BOATCAM

--- start_webserver_scripts/start_debug.bat
+++ start_webserver_scripts/start_debug.bat
@@ -8,11 +8,11 @@ echo.
 
 set NODE_ENV=development
 rem set DEBUG=express:*
-set DEBUG=havneweb:*
+set DEBUG=boatcam:*
 set PORT=3000
 rem set CONFIG=./config/config.json
 
-title HAVNEWEB port: %PORT%, debug mode
+title BOATCAM port: %PORT%, debug mode
 
 pushd ..
 node --debug=5858 ./bin/www
--- start_webserver_scripts/start_release.bat
+++ start_webserver_scripts/start_release.bat
@@ -1,18 +1,18 @@
 @echo off
 setlocal
 echo.
-echo  HAVNEWEB RELEASE MODE
+echo  BOATCAM RELEASE MODE
 echo.
 echo ----------------------------------------------------------------
 echo.
 
 set NODE_ENV=production
 REM set DEBUG=express:*
-set DEBUG=havneweb:*
+set DEBUG=boatcam:*
 set PORT=3000
 rem set CONFIG=./config/config.json
 
-title HAVNEWEB port: %PORT%, release mode
+title BOATCAM port: %PORT%, release mode
 
 pushd ..
 node ./bin/www
