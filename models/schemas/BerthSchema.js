b8e5de7b3ab7c3d4114944a079dc23a9e9cf9ccf
Teis Draiby <teis@teis.net>
2016-03-01 00:52:29 +0100

    #20 Fix Prevent slashes in berth numbers

--- models/schemas/BerthSchema.js
+++ models/schemas/BerthSchema.js
@@ -5,7 +5,13 @@ var BerthSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
-        unique : true
+        unique : true,
+        validate: {
+            validator: function(value) {
+                return /^[a-z0-9-_.]*$/.test(value);
+            },
+            message: 'Field "number" must only contain english lowercase characters and numbers ("{VALUE}")'
+        }
     },
 	owner: String,
     positions: [{ camera: { type: mongoose.Schema.ObjectId, ref: "Camera" }, x: Number, y: Number, zoom: Number}]
