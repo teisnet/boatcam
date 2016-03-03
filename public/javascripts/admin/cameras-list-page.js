54ee74b8e734471ef48a89875d53d5e5cccd97e6
Teis Draiby <teis@teis.net>
2016-03-03 23:57:25 +0100

    Cleanup RestClient initialization
    Unused functions from tutorial removed

--- public/javascripts/admin/cameras-list-page.js
+++ public/javascripts/admin/cameras-list-page.js
@@ -1,34 +1,12 @@
-function loadList (options) {
-    doAjaxCall('GET', options.url, null, function(data) {
-        useTemplate(options.rowTemplate, data, options.rowsContainer);
-    });
-}
+"use strict";
  
-function doAjaxCall(type, url, data, callback) {
-    $.ajax({
-        type: type,
-        url: url,
-        dataType: "json",
-        data: data,
-        success: callback
-    });
-}
- 
-function useTemplate(template, data, container) {
-    if (template) {
-        container.empty();
-        template.tmpl(data).appendTo(container);
-    }
-}
-
-//--------------------
-
 var options = {
-    url: '/api/cameras/',
-    rowTemplate: $('#cameraRowTemplate'),
-    rowsContainer: $('#camerasContainer')
+    name: 'camera',
+    // url: '/api/cameras/',
+    // rowTemplate: $('#cameraRowTemplate'),
+    // rowsContainer: $('#camerasContainer'),
 };
 
 $(document).ready(function(){
-    new RestClient({name:'camera'});
+    var restClient = new RestClient(options);
 });
