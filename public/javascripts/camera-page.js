5a8deea2b93297e5826c87b6bcbb1b123c1695ee
Teis Draiby <teis@teis.net>
2016-04-09 15:29:29 +0200

    Fullscreen button and keyboard shortcut

--- public/javascripts/camera-page.js
+++ public/javascripts/camera-page.js
@@ -27,14 +27,36 @@ $(document).keydown(function(event){
             case 189: // -
                 onMoveCameraKey("zoomOut");
             break;
+			case 70: // F
+				toggleFullScreen($("#player").get(0));
+			break;
         }
+
+		// Ctrl key
+		if (down[17]) {
+			switch(key) {
+				case 70: // F - clashes with 'find'
+					// requestFullScreen($("#player").get(0));
+				break;
+			}
+
+		}
     }
 });
 
 $(document).keyup(function(event){
     var key = event.which || event.keyCode || 0;
     down[key] = null;
-    onMoveCameraKey("Stop");
+	switch(key) {
+		case 38:  // Up
+		case 40:  // Down
+		case 37:  // Left
+		case 39:  // Right
+		case 187: // +
+		case 189: // -
+			onMoveCameraKey("Stop");
+		break;
+	}
 });
 
 $(document).ready(function(){
@@ -80,13 +102,25 @@ $(document).ready(function(){
             'autoplay': true // No effect
         },
         'plugins':{
-            'controls': { display: 'none'},
+            'controls': null, // 'controls: null' will not download controlbar at all
+			/* 'controls': {
+				// display: 'none',
+				all: false,
+				fullscreen: true,
+				backgroundColor: "transparent",
+				backgroundGradient: "none",
+			}, */
             'rtmp':{
                 'url':'http://releases.flowplayer.org/flowplayer.rtmp/flowplayer.rtmp-3.2.13.swf',
                 'netConnectionUrl': config.url
                 }
             },
         'canvas':{"backgroundColor": "#000000",'backgroundGradient':'none'}
+    });
+
+
+    $(".fullscreen").bind("click", function(){
+		toggleFullScreen($("#player").get(0));
     });
 
 });
@@ -129,4 +163,29 @@ function onSnapshot() {
     Camera.snapshot(function(err, result) {
         snapshotWindow.location = "/snapshots/" + result;
     });
+}
+
+
+function toggleFullScreen(element) {
+	var requestMethod = null;
+	var caller = null;
+	if (!document.fullscreenElement &&    // alternative standard method
+		!document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+		requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullscreen;
+		caller = element;
+	}
+	else {
+		requestMethod = document.exitFullscreen || document.msExitFullscreen || document.mozCancelFullScreen || document.webkitExitFullscreen;
+		caller = document;
+	}
+
+	if (requestMethod) { // Native full screen.
+		requestMethod.call(caller);
+	}
+	else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+		var wscript = new ActiveXObject("WScript.Shell");
+		if (wscript !== null) {
+			wscript.SendKeys("{F11}");
+		}
+	}
 }
