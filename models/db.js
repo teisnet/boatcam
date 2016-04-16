587a528a2ebc0e916b3b2ce0b3aab9711876596b
Teis Draiby <teis@teis.net>
2016-04-17 00:25:40 +0200

    Retry database connection if MongoDB isn't started at startup

--- models/db.js
+++ models/db.js
@@ -7,28 +7,12 @@ var db = mongoose.connection;
 //var dbURI = settings.get("DB") || settings.get("db") || 'mongodb://localhost/test';
 var dbURI = 'mongodb://localhost/test';
 
-mongoose.connect(dbURI, function(err){
-	if (err) {
-		console.error("DB: ERROR connecting to database '" +  db.name + "' (please start database before starting server). "  + err);
-		process.exit(0);
-		return;
-	} 
-});
-
+connectWithRetry();
 
 // When successfully connected
 db.on('connected', function () {
 	console.log('DB: Connection "' + db.name + '" succesfully opened.');
-	
-	// db.host // localhost
-	// db.port // 27017
-	//db.name // myDatabase
-
-	// Show database version
-	var admin = new mongoose.mongo.Admin(db.db);
-	admin.buildInfo(function (err, info) {		
-		console.log("DB: MongooDB version = " + info.version + ", Mongoose version = " + mongoose.version );
-	});
+	// db.host = 'localhost', db.port = 27017, db.name = 'myDatabase'
 });
 
 // If the connection throws an error
@@ -46,12 +30,26 @@ db.on('disconnected', function () {
 db.on('reconnect', console.error.bind(console, 'reconnected:'));
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-  db.close(function () {
-    console.log('DB: Default connection "' + db.name + '" disconnected through app termination');
-    process.exit(0);
-  });
-});
+function closeConnectionAndExit() {
+	db.close(function () {
+		console.log('DB: Default connection "' + db.name + '" disconnected through app termination');
+		process.exit(0);
+	});
+}
+
+process.on('SIGTERM', closeConnectionAndExit);
+process.on('SIGINT', closeConnectionAndExit);
+
+function connectWithRetry() {
+	var options = { server: { socketOptions: { keepAlive: 1 } } };
+	return mongoose.connect(dbURI, options, function(err) {
+		if (err) {
+			console.warn("DB: ERROR connecting to database '" +  db.name + "' . Will retry in 5 seconds, please start database. ("  + err + ")");
+			setTimeout(connectWithRetry, 5000);
+			return;
+		}
+	});
+};
 
 
 module.exports = db;
