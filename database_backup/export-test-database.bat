adb4cb133c6da759ae76befd2b574ffd518c2e6b
Teis Draiby <teis@teis.net>
2016-04-24 15:39:20 +0200

    'Users' database table export and import commands

--- database_backup/export-test-database.bat
+++ database_backup/export-test-database.bat
@@ -12,6 +12,7 @@ set PARAMS=--pretty --jsonArray
 set OUT_PATH=mongoexport
 
 @echo on
+mongoexport --db %DB% --collection users --out %OUT_PATH%/users.json %PARAMS%
 mongoexport --db %DB% --collection cameras --out %OUT_PATH%/cameras.json %PARAMS%
 mongoexport --db %DB% --collection berths --out %OUT_PATH%/berths.json %PARAMS%
 mongoexport --db %DB% --collection camera_positions --out %OUT_PATH%/cameraPositions.json %PARAMS%
--- database_backup/import-test-database.bat
+++ database_backup/import-test-database.bat
@@ -12,6 +12,7 @@ set PARAMS=--jsonArray
 set FILE_PATH=mongoexport
 
 @echo on
+mongoimport --db %DB% --collection users --file %FILE_PATH%/users.json %PARAMS%
 mongoimport --db %DB% --collection cameras --file %FILE_PATH%/cameras.json %PARAMS%
 mongoimport --db %DB% --collection berths --file %FILE_PATH%/berths.json %PARAMS%
 mongoimport --db %DB% --collection camera_positions --file %FILE_PATH%/cameraPositions.json %PARAMS%
