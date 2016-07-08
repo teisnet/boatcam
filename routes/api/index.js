256bd4e27d34869cafb3b9225abf072e55591105
Teis Draiby <teis@teis.net>
2016-07-08 09:28:43 +0200

    Enable CORS for for all api routes
    Note that CORS is also enabled for POST, PUT and DELETE, and for all origins
    Authentication and better security should be implemented for production use

--- routes/api/index.js
+++ routes/api/index.js
@@ -12,6 +12,13 @@ router.use(function(req, res, next) {
 });
 */
 
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	//res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type, Authorization");
+	next();
+});
 
 router.use(function (req, res, next) {
 	res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
