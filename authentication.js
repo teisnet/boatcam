38406ffa8f83b2df155fed50216717749a920863
Teis Draiby <teis@teis.net>
2016-08-09 22:19:59 +0200

    Indentation + newline

--- authentication.js
+++ authentication.js
@@ -8,11 +8,11 @@ passport.use(new LocalStrategy(
 	function(req, username, password, done) {
 		User.findOne({ username: username }, function(err, user) {
 			if (err) { return done(err); }
-				if (!user) {
-					return done(null, false, req.flash('message', 'User Not found') );
-				}
-				if (password !== user.password /*!user.validPassword(password)*/) {
-					return done(null, false, req.flash('message', 'Invalid Password') );
+			if (!user) {
+				return done(null, false, req.flash('message', 'User Not found') );
+			}
+			if (password !== user.password /*!user.validPassword(password)*/) {
+				return done(null, false, req.flash('message', 'Invalid Password') );
 			}
 			return done(null, user);
 		});
