9303d0a1eecfc51046aeb123636e55d7f1dd8aab
Teis Draiby <teis@teis.net>
2016-09-06 11:14:31 +0200

    Move 'snapshot' route under authorized 'api' route where it should be

--- routes/api/cameras.js
+++ routes/api/cameras.js
@@ -3,10 +3,26 @@
 var Camera = require("../../models/Camera");
 var errorHandlers = require("./errorHandlers");
 
+var fs = require('fs');
+var path = require("path");
+
 module.exports = function (router) {
+
+	router.get('/snapshots/:filename', function(req, res, next) {
+		let snapshotFilename = req.params.filename;
+		fs.readFile(path.join('./files/', snapshotFilename), function(err, data) {
+			if (err) {
+				res.status(404).send('Snapshot "' + snapshotFilename + '" not found');
+				return;
+			}
+			res.writeHead(200, {'Content-Type': 'image/jpeg'});
+			res.end(data);
+		});
+	})
 
 	// CAMERAS
 	router.route('/cameras')
+
 	// Get all
 	.get(function(req, res, next) {
 		Camera.find({}, function(err, cameras) {
--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,9 @@
 var express = require('express');
 var router = express.Router();
 
-var fs = require('fs');
-var path = require("path");
-
 
 router.get('/', function(req, res, next) {
 	res.render('index');
-});
-
-router.get('/snapshots/:filename', function(req, res, next) {
-	let snapshotFilename = req.params.filename;
-	fs.readFile(path.join('./files/', snapshotFilename), function(err, data) {
-		if (err) {
-			res.status(404).send('Snapshot "' + snapshotFilename + '" not found');
-			return;
-		}
-		res.writeHead(200, {'Content-Type': 'image/jpeg'});
-		res.end(data);
-	});
 });
 
 
