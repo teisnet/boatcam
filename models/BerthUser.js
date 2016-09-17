7155ed648824e09cf1066d52b3a1b3c6f29902ba
Teis Draiby <teis@teis.net>
2016-09-17 18:49:09 +0200

    Add unused sql quries

--- models/BerthUser.js
+++ models/BerthUser.js
@@ -7,7 +7,124 @@ module.exports = function(sequelize, DataTypes) {
 		timestamps: false,
 		// underscored: true,
 		underscoredAll: true,
+		classMethods: {
+			getBerthUsers: function(berthname) {
+				return sequelize.query(queries.getBerthUsers, {
+					bind: { berthname: berthname },
+					type: sequelize.QueryTypes.SELECT
+					// model: BerthUser
+				});
+			},
+
+			getUserBerths: function(username) {
+				return sequelize.query(queries.getUserBerths, {
+					bind: { username: username },
+					type: sequelize.QueryTypes.SELECT
+					// model: BerthUser
+				});
+			},
+
+			updateByName: function(bindParams) {
+				return sequelize.query(queries.editUserBerth, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.UPDATE
+				});
+			},
+
+			removeByName: function(bindParams) {
+				return sequelize.query(queries.removeUserBerth, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.DELETE
+				});
+			},
+
+			createByName: function(bindParams) {
+				return sequelize.query(queries.addUserBerth, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.INSERT
+				});
+			}
+		}
 	});
 
 	return BerthUser;
 };
+
+const queries = {};
+
+queries.getUserBerths = `
+SELECT berths.name, berth_users.status, COALESCE(berth_users.related, false) AS related
+FROM berths
+	LEFT JOIN
+	(
+		SELECT berth_users.berth_id, berth_users.status, true AS related FROM berth_users
+		INNER JOIN users
+		ON berth_users.user_id = users.id AND users.name = $username::varchar
+
+	) AS berth_users
+	ON berths.id = berth_users.berth_id
+`.replace(/\s+/g, ' ');
+
+queries.getBerthUsers = `
+SELECT users.name, berth_users.status, COALESCE(berth_users.related, false) AS related
+FROM users
+	LEFT JOIN
+	(
+		SELECT berth_users.user_id, berth_users.status, true AS related FROM berth_users
+		INNER JOIN berths
+		ON berth_users.berth_id = berths.id AND berths.name = $berthname::varchar
+
+	) AS berth_users
+	ON users.id = berth_users.user_id
+`.replace(/\s+/g, ' ');
+
+queries.addUserBerth = `
+INSERT INTO berth_users (berth_id, user_id, status)
+SELECT p.id, u.id, $status::enum_berth_users_status
+FROM
+(
+	SELECT id
+	FROM berths
+	WHERE name = $berthname::varchar
+) AS p
+CROSS JOIN
+(
+	SELECT id
+	FROM users
+	WHERE name = $username::varchar
+) AS u
+`.replace(/\s+/g, ' ');
+
+queries.editUserBerth = `
+UPDATE berth_users
+SET status = $status::enum_berth_users_status
+WHERE berth_id IN
+(
+	SELECT id AS berth_id
+	FROM berths
+	WHERE name = $berthname::varchar
+)
+AND user_id IN
+(
+	SELECT id AS user_id
+	FROM users
+	WHERE name = $username::varchar
+)
+`.replace(/\s+/g, ' ');
+
+queries.removeUserBerth = `
+DELETE
+FROM berth_users
+WHERE berth_id IN
+(
+	SELECT id AS berth_id
+	FROM berths
+	WHERE name = $berthname::varchar
+)
+AND user_id IN
+(
+	SELECT id AS user_id
+	FROM users
+	WHERE name = $username::varchar
+)
+`.replace(/\s+/g, ' ');
--- models/CameraPosition.js
+++ models/CameraPosition.js
@@ -11,8 +11,125 @@ module.exports = function(sequelize, DataTypes) {
 		// underscored: true,
 		underscoredAll: true,
 		// comment: 'I'm a table comment!'
+		classMethods: {
+			/*getProjectUsers: function(projectname) {
+				return sequelize.query(queries.getProjectUsers, {
+					bind: { projectname: projectname },
+					type: sequelize.QueryTypes.SELECT
+					// model: ProjectUser
+				});
+			},
 
+			getUserProjects: function(username) {
+				return sequelize.query(queries.getUserProjects, {
+					bind: { username: username },
+					type: sequelize.QueryTypes.SELECT
+					// model: ProjectUser
+				});
+			},*/
+
+			updateByName: function(bindParams) {
+				return sequelize.query(queries.editUserProject, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.UPDATE
+				});
+			},
+
+			removeByName: function(bindParams) {
+				return sequelize.query(queries.removeUserProject, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.DELETE
+				});
+			},
+
+			createByName: function(bindParams) {
+				return sequelize.query(queries.addUserProject, {
+					bind: bindParams,
+					type: sequelize.QueryTypes.INSERT
+				});
+			}
+		}
 	});
 
 	return ProjectUser;
 };
+
+const queries = {};
+/*
+queries.getUserProjects = `
+SELECT projects.name, project_users.status, COALESCE(project_users.related, false) AS related
+FROM projects
+	LEFT JOIN
+	(
+		SELECT project_users.project_id, project_users.status, true AS related FROM project_users
+		INNER JOIN users
+		ON project_users.user_id = users.id AND users.name = $username::varchar
+
+	) AS project_users
+	ON projects.id = project_users.project_id
+`.replace(/\s+/g, ' ');
+
+queries.getProjectUsers = `
+SELECT users.name, project_users.status, COALESCE(project_users.related, false) AS related
+FROM users
+	LEFT JOIN
+	(
+		SELECT project_users.user_id, project_users.status, true AS related FROM project_users
+		INNER JOIN projects
+		ON project_users.project_id = projects.id AND projects.name = $projectname::varchar
+
+	) AS project_users
+	ON users.id = project_users.user_id
+`.replace(/\s+/g, ' ');
+*/
+
+queries.addUserProject = `
+INSERT INTO project_users (project_id, user_id, status)
+SELECT p.id, u.id, $status::enum_project_users_status
+FROM
+(
+	SELECT id
+	FROM projects
+	WHERE name = $projectname::varchar
+) AS p
+CROSS JOIN
+(
+	SELECT id
+	FROM users
+	WHERE name = $username::varchar
+) AS u
+`.replace(/\s+/g, ' ');
+
+queries.editUserProject = `
+UPDATE project_users
+SET status = $status::enum_project_users_status
+WHERE project_id IN
+(
+	SELECT id AS project_id
+	FROM projects
+	WHERE name = $projectname::varchar
+)
+AND user_id IN
+(
+	SELECT id AS user_id
+	FROM users
+	WHERE name = $username::varchar
+)
+`.replace(/\s+/g, ' ');
+
+queries.removeUserProject = `
+DELETE
+FROM project_users
+WHERE project_id IN
+(
+	SELECT id AS project_id
+	FROM projects
+	WHERE name = $projectname::varchar
+)
+AND user_id IN
+(
+	SELECT id AS user_id
+	FROM users
+	WHERE name = $username::varchar
+)
+`.replace(/\s+/g, ' ');
