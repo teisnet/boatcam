13deea43bd543424ca47f435eec11f7f8e4f594a
Teis Draiby <teis@teis.net>
2016-09-21 16:52:59 +0200

    Missing comma in models/User.js

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ module.exports = function(sequelize, DataTypes) {
 		role: {
 			type: DataTypes.ENUM('Administrator', 'Editor', 'User'),
 			defaultValue: 'User'
-		},
+		}
 	}, {
 		// TODO: Don't serialize password'
 		// underscored: true,
